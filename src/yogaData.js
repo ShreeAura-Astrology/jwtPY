8547eeb0a96aaf168ba6c29754d66f455d0e5591
ShreeAura-Astrology <recharge.ashu@gmail.com>
2026-01-16 14:23:46 +0000

    feat: enhance Mercury yogas with additional categories and health-related insights

---
+++ src/yogaData.js
@@ -0,0 +1,63 @@
+var yogasData = {
+  "bnn_yogas_list": {
+    "spiritual_and_philosophical_yogas": {
+      "jeevaathmaa_yoga": "Jupiter + Sun.",
+      "datattreya_yoga": "Jupiter + Mercury + Sun.",
+      "kala_bhairava_yoga": "Saturn + Rahu.",
+      "chitta_nirodha_yoga": "Moon + Ketu, which grants control over the mind.",
+      "pranihita_yoga": "Mercury + Ketu, signifying concentrated thought.",
+      "siva_nayana_yoga": "Moon, Jupiter, and Mercury placed in the 3rd house from each other.",
+      "guru_gangadhara_yoga": "Saturn + Moon + Jupiter.",
+      "sanyas_yoga_monk_nun": "Saturn + Moon + Ketu."
+    },
+    "wealth_and_success_yogas": {
+      "ashtalakshmi_yoga": "Jupiter or Venus + Saturn + Rahu.",
+      "bruhat_bahu_dhana_yoga": "Venus + Rahu.",
+      "multi_millionaire_yoga": "Saturn + Venus + Mercury + Rahu.",
+      "maha_bhagya_yoga": "Saturn + Jupiter.",
+      "bhagya_yoga": "Venus + Saturn.",
+      "maharaja_yoga": "Exalted Sun in conjunction with Jupiter and Ketu.",
+      "special_raja_yoga": "Sun + Mars + Jupiter + Mercury.",
+      "dharma_karmathipathi_yoga": "Jupiter + Saturn."
+    },
+    "knowledge_and_arts_yogas": {
+      "saraswathi_kalanidhi_yoga": "Jupiter + Venus + Mercury.",
+      "budha_adhitya_yoga": "Sun + Mercury.",
+      "brammaanda_buddhi_yoga": "Mercury + Rahu.",
+      "sangeeth_kala_guru_yoga": "Moon, Mercury, and Venus with Jupiter and Saturn.",
+      "padma_yoga": "Venus + Ketu when both are in watery signs."
+    },
+    "planetary_interaction_yogas": {
+      "tejo_agni_yoga": "Sun + Mars.",
+      "kemadruma_yoga": "Occurs when there are no planets in the 12th or 2nd houses relative to the Moon.",
+      "thorn_yoga_mullu": "Mars + Ketu.",
+      "agni_marutha_yoga": "Mars + Saturn.",
+      "sanjeevani_yoga_athi_kaama": "Jupiter + Venus.",
+      "vasikara_yoga_kalanka": "Jupiter or Venus + Moon.",
+      "brighu_mangala_yoga": "Venus + Mars.",
+      "guru_mangala_yoga": "Jupiter + Mars.",
+      "arthanariswara_yoga": "Sun + Moon.",
+      "angaraka_yoga": "Mars + Rahu.",
+      "dwikalatra_yoga": "Jupiter or Sun + Venus + Mercury.",
+      "aputra_yoga": "Sun + Rahu with Venus + Ketu, or Sun + Ketu with Venus + Rahu."
+    },
+    "health_and_disease_yogas": {
+      "cancer_yoga": "Saturn + Rahu + Mars or Mars + Moon + Rahu.",
+      "spinal_cord_problems": "Rahu + Mercury + Mars.",
+      "thyroid_throat_issues": "Mercury + Moon, sometimes with Venus.",
+      "ear_disease": "Rahu + Mercury + Jupiter.",
+      "migraine_headache_yoga": "Sun + Moon + Rahu + Saturn.",
+      "asthma_breathing_issues": "Saturn + Moon + Ketu.",
+      "blood_pressure_yoga": "Jupiter linked with Mars, Ketu, or a combination of Mars and Rahu/Ketu.",
+      "arthritis_joint_pain": "Sun + Saturn or Mars + Saturn.",
+      "psychological_problems": "Moon + Rahu or Moon + Ketu."
+    },
+    "career_combinations": {
+      "surgeon_doctor": "Sun + Mars or Sun + Ketu.",
+      "science_maths": "Venus + Mars + Mercury.",
+      "it_sector": "Moon + Mercury.",
+      "tantric_yoga": "Mercury + Mars + Rahu + Saturn.",
+      "pilgrimage_travel_business": "Mercury + Moon + Ketu."
+    }
+  }
+};
